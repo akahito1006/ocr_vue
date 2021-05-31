b258355b2f17b2a534bb6d602d1a3b6726960e2a
akahito1006 <nekodora5k@gmail.com>
2021-05-31 11:08:08 +0900

    single file components

---
+++ note/P2C2/sfc/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import HomeLink from "./components/HomeLink.vue"
+
+export default {
+  name: 'App',
+  components: {
+    HomeLink
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <nav>
+      <HomeLink />
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </nav>
+    <p>Welcome to our <HomeLink /> page!</p>
+  </div>
+</template>
