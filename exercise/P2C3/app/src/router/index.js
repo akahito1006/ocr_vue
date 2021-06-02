641aff702ee970166458492438befc40fb9c67ab
akahito1006 <nekodora5k@gmail.com>
2021-06-02 15:50:39 +0900

    route

--- exercise/P2C3/app/src/router/index.js
+++ exercise/P2C3/app/src/router/index.js
@@ -1,7 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
 // ここ
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -25,8 +28,8 @@ const routes = [
   }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
