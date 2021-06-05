b67bc3bbe6b5cc64f6f51863ccaa0f589617bf74
akahito1006 <nekodora5k@gmail.com>
2021-06-06 01:15:03 +0900

    how to if and to decide whether its even or odd

---
+++ exercise/P2C4/app/src/views/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p>Address: {{ address }}</p>
+  <p>Phone: {{ phone }}</p>
+  <p>Email: {{ email }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  props: ['address', 'phone', 'email']
+}
+</script>
+
+<style></style>
---
+++ exercise/P2C4/app/src/views/Home.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="app" class="app">
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
+      and french pastries! Give you morning a warm start or treat yourself in
+      the middle of the day. Our butter is imported from local farmers in
+      France. Once you take your first bite, you will see why everyone can't get
+      enough!
+    </p>
+
+    <section class="menu">
+      <h2>Menu</h2>
+      <MenuItem
+        v-for="item in simpleMenu"
+        :addToShoppingCart="addToShoppingCart"
+        :name="item.name"
+        :price="item.price"
+        :image="item.image"
+        :quantity="item.quantity"
+        :inStock="item.inStock"
+        :key="item.name"
+      />
+    </section>
+
+    <aside class="shopping-cart">
+      <h2>Shopping Cart: {{ shoppingCart }} items</h2>
+    </aside>
+
+    <h2>Contact Us</h2>
+    <Contact
+      :address="address"
+      :phone="phone"
+      :email="email"
+    />
+    <p>Hours:</p>
+    <ul>
+      <li>M-F: 06:00 to 16:00</li>
+      <li>Saturday: 07:00 to 14:00</li>
+      <li>Sunday: 07:00 to 12:00</li>
+    </ul>
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import MenuItem from '../components/MenuItem'
+import Contact from './Contact'
+export default {
+  name: 'App',
+  components: {
+    MenuItem,
+    Contact
+  },
+  data() {
+    return {
+      address: '9274 Beurre Ave, Paris, France',
+      email: 'hello@cafewithavue.bakery',
+      phone: '+08 88 88 88 88 88',
+      restaurantName: 'Cafe with A Vue',
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: 'Crossiant',
+          price: 10,
+          image: {
+            source: '/images/crossiant.jpg',
+            alt: 'A crossiant'
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: 'French Baguette',
+          price: 20,
+          image: {
+            source: '/images/french-baguette.jpeg',
+            alt: 'Four French Baguettes'
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: 'Éclair',
+          price: 30,
+          image: {
+            source: '/images/eclair.jpg',
+            alt: 'Chocolate Éclair'
+          },
+          inStock: false,
+          quantity: 1
+        }
+      ]
+    }
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear()
+      return `Copyright ${this.restaurantName} ${currentYear}`
+    }
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
